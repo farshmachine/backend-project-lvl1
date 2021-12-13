2c35afacd13ffa6ce054479495ff267ea30f2497
Mike Makarevich <56134813+farshmachine@users.noreply.github.com>
2021-12-13 11:49:16 -0800

    Update lint.yml

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSYnc from 'readline-sync';
 
-export function greeting() {
+export default function greeting() {
   console.log('Welcone to the Brain Games');
   const username = readlineSYnc.question('May i have you name? ');
   console.log(`Hello, ${username}!`);
 }
-
