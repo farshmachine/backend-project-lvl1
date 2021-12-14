9d43ee7a5d4dd29d68e958ee8ae454dcb6fb7f73
farshmachine <m4ghty@yandex.ru>
2021-12-14 11:36:41 -0800

    fixed error in progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,11 +10,18 @@ function getQuestion() {
 
 function getCorrectAnswer(question) {
   const progression = question.split(' ');
-  const [first, second] = progression;
+  const filteredProgression = progression.filter((el) => el !== '..');
+  const [first, second] = filteredProgression;
   const step = Number(second) - Number(first);
   const removedItemIndex = progression.findIndex((el) => el === '..');
+  const previousItem = progression[removedItemIndex - 1];
+  const nextItem = progression[removedItemIndex + 1];
 
-  return (Number(progression[removedItemIndex - 1]) + step).toString();
+  if (previousItem) {
+    return String(Number(previousItem) + step);
+  }
+
+  return String(Number(nextItem) - step);
 }
 
 function showRules() {
