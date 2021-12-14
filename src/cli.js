768ede069e2e6d6293bb3c388fea5ee58cd67515
farshmachine <m4ghty@yandex.ru>
2021-12-14 11:25:32 -0800

    error fix

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSYnc from 'readline-sync';
 
 export default function greeting() {
-  const username = readlineSYnc.question('Welcome to the Brain Games!\nMay I have you name? ');
+  const username = readlineSYnc.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${username}!`);
 
   return username;
